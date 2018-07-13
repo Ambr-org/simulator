96bfc3295dac6047b92351975e6a28984c034297
Kimi Kan <k32459871@126.com>
2018-07-13 16:09:21 +0800

    fix format

--- node/protocol/config.go
+++ node/protocol/config.go
@@ -1,3 +1,7 @@
+///Copyright (c) 2018 Ambr project
+///Written by KimiKan
+///Distributed under the MIT software license, see the accompanying
+///file COPYING or http://www.opensource.org/licenses/mit-license.php.
 package protocol
 
 import (
@@ -8,21 +12,21 @@ import (
 
 /*
 <app>
-    <general>
-        <Item1>xx</Item1>
-    </general>
+	<general>
+		<Item1>xx</Item1>
+	</general>
 
-		<network>
+	<network>
+		<port>7777</port>
+		<maxpeers>5</maxpeers>
+	</network>
+	<Seeds>
+		<seed>
+			<dns></dns>
+			<ip>192.168.1.2</ip>
 			<port>7777</port>
-			<maxpeers>5</maxpeers>
-		</network>
-    <Seeds>
-        <seed>
-						<dns></dns>
-						<ip>192.168.1.2</ip>
-						<port>7777</port>
-        </seed>
-    </disks>
+		</seed>
+	</disks>
 	<cache></cache>
 </app>
 */
