0dc173a7522a69677938cf865c0a91d9dbc2582b
Kimi Kan <k32459871@126.com>
2018-08-02 18:37:00 +0800

    network

--- node/protocol/network.go
+++ node/protocol/network.go
@@ -1,4 +1,21 @@
+///Copyright (c) 2018 Ambr project
+///Written by KimiKan
+///Distributed under the MIT software license, see the accompanying
+///file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 package protocol
 
-//TODO:
-//noise..........
+type NetworkHelper struct {
+}
+
+func NewNetworkHelper() *NetworkHelper {
+	return &NetworkHelper{}
+}
+
+func (p *NetworkHelper) Publish(msg string) error {
+	return nil
+}
+
+func (p *NetworkHelper) Vote(msg string) error {
+	return nil
+}
